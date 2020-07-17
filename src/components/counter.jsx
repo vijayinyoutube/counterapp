b0c504824ad4b564fde4177c956462b2afc2f421
Vijay <vijaythevirtue@gmail.com>
2020-07-17 19:59:21 +0530

    initializing counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
 
   handleIncreament = (product) => {
     this.setState({
-      count: (this.state.count += 1),
+      value: (this.state.value += 1),
     });
   };
   render() {
@@ -25,13 +25,13 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
